0a561381360d4446172ab502401fa86afc6619ca
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-22 09:17:08 +0400

    add Task 9 - return the length of the longest adjacent of characters

---
+++ src/Task9MaxBlock.java
@@ -0,0 +1,29 @@
+public class Task9MaxBlock {
+    public static void main(String[] args){
+        System.out.println(maxBlock("hoopla"));
+        System.out.println(maxBlock("abbCCCddBBBxx"));
+        System.out.println(maxBlock(""));
+        System.out.println(maxBlock(" "));
+    }
+    public static int maxBlock(String text){
+        int count = 1;
+        int currentCount=1;
+        if(text.length() ==0){
+            return 0;
+        }
+        for(int i=1; i< text.length(); i++){
+            char chr1 = text.charAt(i);
+            char chr2 =text.charAt(i-1);
+            if(chr1==chr2){
+                currentCount++;
+                if(currentCount > count){
+                    count=currentCount;
+                }
+            }else{
+                    currentCount=1;
+                }
+            }
+
+        return count;
+    }
+}
