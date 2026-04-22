53834a39b638c71e0039abf75fe91ff6dd8ea718
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-22 09:53:01 +0400

    add Task 11 - add not to the string after is

---
+++ src/Task11NotReplace.java
@@ -0,0 +1,24 @@
+public class Task11NotReplace {
+    public static void main(String[] args){
+        System.out.println(notReplace("is test"));
+        System.out.println(notReplace("is-is"));
+        System.out.println(notReplace("This is right"));
+    }
+
+    public static String notReplace(String text){
+        String result = "";
+        for(int i = 0; i < text.length(); i++){
+            if(i < text.length() - 1 &&
+                    text.charAt(i) == 'i' &&
+                    text.charAt(i + 1) == 's' &&
+                    (i == 0 || !Character.isLetter(text.charAt(i - 1))) &&
+                    (i + 2 >= text.length() || !Character.isLetter(text.charAt(i + 2)))){
+                result = result + "is not";
+                i++; // skip 's'
+            } else {
+                result = result + text.charAt(i);
+            }
+        }
+        return result;
+    }
+}
