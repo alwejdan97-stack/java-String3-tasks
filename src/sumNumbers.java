2661aeadba61d30f34c9978b9f31eaa65774ef23
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-22 13:15:32 +0400

    add Task 10 - return the sum of total number at the same decimal places

--- src/sumNumbers.java
+++ src/sumNumbers.java
@@ -1,4 +1,4 @@
-public class Task10SumNumbers {
+public class sumNumbers {
     public static void main(String[] args){
         System.out.println(sumNumbers("abc123xyz"));
         System.out.println(sumNumbers("aa11b33"));
