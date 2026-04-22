f6f06803855a294a30c601f78c712cb8581f1900
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-22 10:00:42 +0400

    add Task 4 - check if the string contain gg or not

--- src/Task4gHappy.java
+++ src/Task4gHappy.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Task4gHappy {
+    public static void main(String[] args){
+        System.out.println("check if gg appear in the text or not: "+ gHappy("egg"));
+        System.out.println("check if gg appear in the text or not: "+ gHappy("xxggyygxx"));
+        System.out.println("check if gg appear in the text or not: "+ gHappy("xxgxx"));
+    }
+    public static boolean gHappy(String text){
+        text = text.toLowerCase();
+        for(int i=0; i<text.length(); i++){
+            if(text.charAt(i)=='g'){
+                if(!(i> 0 && text.charAt(i-1)=='g') && !(i<text.length()-1&& text.charAt(i+1)=='g')){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
